5822f11960b2a698f86a64100d0b6e434aaf5aa6
Cody Hastings <cody@hastings.email>
2021-10-22 09:40:56 -0500

    step 13 complete

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,8 +7,23 @@ public class HelloWorld {
         System.out.println(myString);
         double MyNumber = 3.14;
         System.out.println(MyNumber);
-        int x = 5;
-        System.out.println(++x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
         System.out.println(x);
+        System.out.println(y);
+
     }
 }
