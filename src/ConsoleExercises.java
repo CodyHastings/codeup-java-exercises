6903c113d343c3c70fdafa4bcda9475bc133094b
Cody Hastings <cody@hastings.email>
2021-10-22 16:00:20 -0500

    store 3 words and print

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class ConsoleExercises {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
 //        Exercises
 //        For the following exercises, create a new class named ConsoleExercises with a main method like the one in your HelloWorld class.
 //
@@ -9,17 +11,29 @@ public class ConsoleExercises {
 //
         double pi = 3.14159;
 //        Write some Java code that uses the variable pi to output the following:
-        System.out.format("The value of pi is approximately %.2f ", pi);
+        System.out.format("The value of pi is approximately %.2f \n", pi);
 //
 //        The value of pi is approximately 3.14.
 //                Don't change the value of the variable; instead, reference one of the links above and use System.out.format to accomplish this.
 //
 //        Explore the Scanner Class.
 //        Prompt a user to enter a integer and store that value in an int variable using the nextInt method.
-//
+//        System.out.format("Enter an integer please: ");
+//        int myInt = scanner.nextInt();
+
+//        System.out.println(myInt);
 //        What happens if you input something that is not an integer?
 //        Prompt a user to enter 3 words, and store each of them in a separate variable. Then, display them back in the console, each on a newline.
-//
+        System.out.format("Enter a word please: ");
+        String firstWord = scanner.nextLine();
+
+        System.out.format("Another word: ");
+        String secondWord = scanner.nextLine();
+
+        System.out.format("And Another word: ");
+        String thirdWord = scanner.nextLine();
+
+        System.out.printf("Your first word entered was %s. Your second word was %s. Also your third word was %s", firstWord, secondWord, thirdWord);
 //                What happens if you enter less than 3 words?
 //                What happens if you enter more than 3 words?
 //                Prompt a user to enter a sentence, then store that sentence in a String variable using the next method. Then, display that sentence back to the user.
