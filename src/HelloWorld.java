c69a5c5182ab38255bcf30ce9299e3387aa9749b
Cody Hastings <cody@hastings.email>
2021-10-22 09:20:46 -0500

    Step 2, printed a string

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello, World!");
+        System.out.println("Hello, World!");
         int myFavoriteNumber = 42;
         System.out.println(myFavoriteNumber);
+        String myString = "Dune was great";
+        System.out.println(myString);
+
     }
 }
