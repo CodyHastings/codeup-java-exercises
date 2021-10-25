cabd9e0b95141c987ec80fc7d3d64520de8bd3fe
Cody Hastings <cody@hastings.email>
2021-10-25 11:24:15 -0500

    first part of exercise complete, do while

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,39 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+//        Question 1
+//        int i = 5;
+//
+//        while (i <= 15) {
+//            System.out.println(i);
+//            i += 1;
+//        }
+//        first do While
+//        int i = 0;
+//
+//        do {
+//            System.out.println(i);
+//            i += 2;
+//        } while(i <= 100);
+
+//      Refactor to count backwards from 100 by 5s
+//        int i = 100;
+//
+//        do {
+//            System.out.println(i);
+//            i -= 5;
+//        } while(i >= -10);
+//        Refactor to print squares up to 1 million
+//        long i = 2;
+//        do {
+//            System.out.println(i);
+//            i *= i;
+//        } while(i <= 1000000);
+
+
+
+//        for loop refactor
+
+
+    }
+
+}
