435b5796a4f8920a13535d47eda8101f388b28ac
Cody Hastings <cody@hastings.email>
2021-10-25 12:30:42 -0500

    working through adding continue to number 3

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 //        Question 1
 //        int i = 5;
 //
@@ -33,7 +36,73 @@ public class ControlFlowExercises {
 
 //        for loop refactor
 
+//        for (int i = 5; i <= 15; i++){
+//            System.out.println(i);
+//        }
+//        for (int i = 0; i <= 100; i+= 2){
+//            System.out.println(i);
+//        }
+//        for (int i = 100; i >= -10; i-= 5){
+//            System.out.println(i);
+//        }
+//        for (long i = 2; i <= 1000000; i*= i){
+//            System.out.println(i);
+//        }
 
-    }
+//        FIZZ BUZZ!!!!!!!
 
-}
+//        for (int i = 1; i <= 100; i+= 1){
+//            if(i % 15 == 0){
+////                System.out.println(i);
+//                System.out.println("FizzBuzz!");
+//            } else if(i % 3 == 0){
+////                System.out.println(i);
+//                System.out.println("Fizz");
+//            }
+//            else if(i % 5 == 0){
+////                System.out.println(i);
+//                System.out.println("Buzz");
+//            } else
+//            System.out.println(i);
+//        }
+
+
+//        3 Table of powers
+//        System.out.print("What number would you like to go up to? ");
+//        int maxInt = scanner.nextInt();
+//        System.out.println("\nHere is your table! \n");
+//        System.out.println("number | squared | cubed \n------ | ------- | ----- ");
+//        for (int i = 0; i <= maxInt; i++){
+//            int squared = i * i;
+//            int cubed = i * i * i;
+//
+//            System.out.format("%-7d| %-8d| %-6d\n", i, squared, cubed);
+//        }
+
+        System.out.print("What number would you like to go up to? ");
+        int maxInt = scanner.nextInt();
+        System.out.println("\nHere is your table! \n");
+        System.out.println("number | squared | cubed \n------ | ------- | ----- ");
+        boolean continbuebool = false;
+        do {
+            for (int i = 0; i <= maxInt; i++){
+                int squared = i * i;
+                int cubed = i * i * i;
+
+                System.out.format("%-7d| %-8d| %-6d\n", i, squared, cubed);
+
+                System.out.print("Continue? y/n ");
+
+            } String continuePrompt = scanner.nextLine();
+            if (continuePrompt == "y"){
+               boolean continuebool = true;
+            } else{
+                boolean continbuebool = false;
+            }
+        } while(continuebool);
+
+
+
+    } //main
+
+} //Class
